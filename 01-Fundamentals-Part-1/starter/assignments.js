03c2de89cdc852709731d26eb26be30f88ba5c56
Kym Augusto <kymaugustops@gmail.com>
2026-03-22 11:31:59 -0300

    Exercicios Aula 11

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,7 +1,15 @@
 let country = "Brazil"
 let continent = "South America"
-let population = "213"
+let population = 213
+let isIsland = false;
+let language;
+
 
 console.log(country);
 console.log(continent);
 console.log(population);
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
