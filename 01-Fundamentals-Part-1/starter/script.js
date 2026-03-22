54feb9d050f8c5238d990f06f1504a0471315296
Kym Augusto <kymaugustops@gmail.com>
2026-03-22 10:48:16 -0300

    aula 9

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,25 @@
 let js = "amazing";
-if (js === "amazing") alert ("JavaScript is FUN!");
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log("23");
+
+let firstName = "Matilda"
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonasMatilda = "JM";
+let $function = 27;
+
+let person = "Jonas"
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "Programmer";
+let job2 = "Teacher";
+
+console.log(myFirstJob);
