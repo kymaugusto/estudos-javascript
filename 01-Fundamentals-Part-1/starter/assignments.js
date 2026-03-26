2bb35244009107a0279a6c94eca173a2bb11d6ac
Kym Augusto <kymaugustops@gmail.com>
2026-03-26 16:09:27 -0300

    Exercices from lesson 13

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -8,6 +8,7 @@ language = "Portuguese";
 
 
 
+
 console.log(country);
 console.log(continent);
 console.log(population);
@@ -16,3 +17,24 @@ console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
+
+console.log(population / 2);
+
+population++;
+
+console.log(population > 6);
+
+console.log(population < 33);
+
+const description = 
+    country + 
+    " is in " + 
+    continent + 
+    ", and its " + 
+    population + 
+    " million people speak " + 
+    language;
+
+console.log(description);
+
+
