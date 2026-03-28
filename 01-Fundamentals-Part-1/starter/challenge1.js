8674cf6888292dc6de35893701a031fc0f23b6aa
Kym Augusto <kymaugustops@gmail.com>
2026-03-28 11:21:56 -0300

    Challenge 1 and lesson 17

---
+++ 01-Fundamentals-Part-1/starter/challenge1.js
@@ -0,0 +1,10 @@
+let markMass = 78;
+const markHeight = 1.69;
+let johnMass = 92;
+const johnHeight = 1.95;
+
+const markBMI = markMass / markHeight ** 2;
+const johnBMI = johnMass / johnHeight ** 2;
+const markHigherBMI = markBMI > johnBMI;
+
+console.log(markBMI, johnBMI, markHigherBMI);
