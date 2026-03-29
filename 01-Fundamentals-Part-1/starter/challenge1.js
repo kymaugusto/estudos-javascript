2a1a9223d1b0043ad6e83dbd0722896cd306bc22
Kym Augusto <kymaugustops@gmail.com>
2026-03-29 11:22:58 -0300

    Challenge 2

--- 01-Fundamentals-Part-1/starter/challenge1.js
+++ 01-Fundamentals-Part-1/starter/challenge1.js
@@ -1,4 +1,4 @@
-
+/*
 let markMass = 78;
 const markHeight = 1.69;
 let johnMass = 92;
@@ -6,10 +6,10 @@ const johnHeight = 1.95;
 
 const markBMI = markMass / markHeight ** 2;
 const johnBMI = johnMass / johnHeight ** 2;
-const markHigherBMI = markBMI > johnBMI;
 
 if(markBMI > johnBMI){
     console.log(`Mark's BMI ${markBMI} is higher than John's ${johnBMI}`);
 }else {
     console.log(`John's BMI ${johnBMI} is higher than Mark's ${markBMI}`);
 }
+*/
