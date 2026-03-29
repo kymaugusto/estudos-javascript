19f390832bf2fe6dc876bfc5135fbcbcba96eb0a
Kym Augusto <kymaugustops@gmail.com>
2026-03-29 12:14:50 -0300

    Exercise 7

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -37,3 +37,9 @@ else {
     console.log(`${country}'s population is ${33 - population} million below average`);
 }
 */
+
+console.log('9' - '5'); // -> 4
+console.log('19' - '13' + '17'); // -> 617
+console.log('19' - '13' + 17); // -> 23
+console.log('123' < 57); // -> False
+console.log(5 + 6 + '4' + 9 - 4 - 2); // -> 1143
