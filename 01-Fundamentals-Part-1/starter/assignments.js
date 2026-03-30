4caf2faebbbc2a3f8c1a412bbc27b5d75cfac917
Kym Augusto <kymaugustops@gmail.com>
2026-03-30 10:42:26 -0300

    Lesson 21

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -36,10 +36,10 @@ if(population > 33) {
 else {
     console.log(`${country}'s population is ${33 - population} million below average`);
 }
-*/
 
 console.log('9' - '5'); // -> 4
 console.log('19' - '13' + '17'); // -> 617
 console.log('19' - '13' + 17); // -> 23
 console.log('123' < 57); // -> False
 console.log(5 + 6 + '4' + 9 - 4 - 2); // -> 1143
+*/
